73782994e0d2e04afa99ec4edd0772a69812ba55
gabrieltodino <gabrieltodino08@gmail.com>
2021-08-11 16:56:56 -0300

    motores

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,8 +1,13 @@
 String datafromUser="";
+int IN5 = 5;
+int IN6 = 6;
+byte speed = 150;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(7 , OUTPUT );
+  pinMode(IN5, OUTPUT);
+  pinMode(IN6, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -15,8 +20,20 @@ void loop() {
 
   if(datafromUser == "acender led"){
     digitalWrite( 7 , HIGH );
-  }
-  else if(datafromUser == "apagar led"){
+  }else if(datafromUser == "apagar led"){
     digitalWrite( 7, LOW);
+  }else if(datafromUser == "ligar motor"){
+    avancar(speed);
+  }else if(datafromUser == "aumentar velocidade"){
+    speed += 25;
+    avancar(speed);
+  }else if(datafromUser == "diminuir velocidade"){
+    speed -= 25;
+    avancar(speed);
   }
+}
+
+void avancar(byte power) {
+  analogWrite(IN5, power);
+  digitalWrite(IN6, LOW);
 }
